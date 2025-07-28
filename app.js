a36ce43c8c5e5fe6d0529dbe1bf8bd939001ab74
jegadeshpalanisamy-sparkout <jegadesh.palanisamy@sparkouttech.com>
2025-07-28 22:42:42 +0530

    get the data from request

--- app.js
+++ app.js
@@ -26,4 +26,20 @@ app.use('/second-page', (req, res, next) => {
 //     console.log('Server running on http://localhost:3000');
 // });
 
+
+//using body parser to get payload
+const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/get-payloads-using-body-parser',(req,res,next)=>{
+    res.send('<form action="/upload-name" method="POST"><input type="text" name="name"><button type="submit">Send</button></form>');
+})
+
+app.post('/upload-name', (req, res, next) => {
+    console.log('middleware 4');
+    console.log(req.body);
+    res.send('<h1>Hello from second-page!</h1>');
+});
+
 app.listen(3000);
